e913653737b1cbdd10af4f9aabc4e97bf6cff0e9
FinarioEU <finario@outlook.com>
2025-10-21 12:19:59 +0200

    Update main.ts

--- apps/API/src/main.ts
+++ apps/API/src/main.ts
@@ -2,30 +2,18 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
-import { ThrottlerGuard } from '@nestjs/throttler';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Security Header
   app.use(helmet());
+  app.enableCors({ origin: '*', credentials: false });
 
-  // CORS (Frontend kann später hier eingetragen werden)
-  app.enableCors({
-    origin: '*',
-    credentials: false,
-  });
-
-  // Globales Rate-Limit
-  app.useGlobalGuards(new ThrottlerGuard());
-
-  // Validation für DTOs (später)
+  // Globale Validation
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const port = Number(process.env.PORT) || 4000;
   await app.listen(port, '0.0.0.0');
-  console.log(`Server läuft auf Port: ${port}`);
+  console.log(✅ Server läuft auf Port: ${port});
 }
-
 bootstrap();
