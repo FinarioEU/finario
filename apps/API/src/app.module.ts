b212b8298f0449b01dabc30a2fe8720806445a93
FinarioEU <finario@outlook.com>
2025-10-21 11:32:40 +0200

    Update app.module.ts

--- apps/API/src/app.module.ts
+++ apps/API/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
+import { VersionController } from './version.controller.js';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, VersionController],
   providers: [],
 })
 export class AppModule {}
