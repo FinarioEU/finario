1cb081fc3f92e9db459facb649356f618ddf7078
FinarioEU <finario@outlook.com>
2025-10-21 11:29:54 +0200

    Update app.controller.ts

--- apps/API/src/app.controller.ts
+++ apps/API/src/app.controller.ts
@@ -2,6 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  @Get('/')
+  getRoot() {
+    return { status: 'ok', message: 'Finario API is running 🚀' };
+  }
+
   @Get('/health')
   getHealth() {
     return { status: 'ok', message: 'Finario API is running 🚀' };
