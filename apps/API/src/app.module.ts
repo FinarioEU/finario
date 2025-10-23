79abfe99dc6a3707dfcde4217912914c02c038b6
FinarioEU <finario@outlook.com>
2025-10-23 11:43:49 +0200

    Update app.module.ts

--- apps/API/src/app.module.ts
+++ apps/API/src/app.module.ts
@@ -1,23 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
-
-import { AppController } from './app.controller.js';
-import { VersionController } from './version.controller.js';
+import { AppController } from './app.controller';
+import { VersionController } from './version.controller';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    ThrottlerModule.forRoot([{
-      name: 'default',
-      ttl: 60_000,
-      limit: 60,
-    }]),
-  ],
   controllers: [AppController, VersionController],
-  providers: [
-    { provide: APP_GUARD, useClass: ThrottlerGuard },
-  ],
+  providers: [],
 })
 export class AppModule {}
