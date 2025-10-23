9b1bf1a547fa257306a1fc154562d231a8fb04f3
FinarioEU <finario@outlook.com>
2025-10-23 16:32:17 +0200

    Update app.module.ts

--- apps/API/src/app.module.ts
+++ apps/API/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { VersionController } from './version.controller';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { FinanceModule } from './finance/finance.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule, FinanceModule],
   controllers: [AppController, VersionController],
-  providers: []
+  providers: [],
 })
 export class AppModule {}
